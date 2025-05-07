c9aeacf4ec7ba9a68cca765de0352b88320aa5b5
Raj <ssukhdevsingh0128@gmail.com>
2025-05-07 15:43:44 +0530

    added get mapping for sayHi

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,6 +10,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello");
+	}
+
+	@GetMapping("/sayHi")
+	public String sayHello(){
+		return "Hello from my app";
 	}
 
 }
